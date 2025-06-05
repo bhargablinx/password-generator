a7491f93c00ceef9c2bf01bb5f6231c00539dd0d
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-05 09:53:55 +0000

    pass dynamicaly generated

--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ const passLenSlider = document.querySelector(".pass-len-slider");
 function displayPassLength(length) {
     document.querySelector(".pass-length-holder").textContent = length;
     passwordLength = Number(length);
+    generatePassword();
 }
 
 function generatePassword() {
@@ -16,13 +17,16 @@ function generatePassword() {
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numbers = "1234567890";
     const symbols = `!#$&@%*`;
+    if (upperCaseAllowed) {
+        passOptions = "abcdabcdefghiJKLMNOPQRSTUVWXYZ";
+    }
     if (numAllowed) passOptions += numbers;
-    if (upperCaseAllowed) passOptions += upperCase;
     if (symbolsAllowed) passOptions += symbols;
     for (let i = 0; i < passwordLength; i++) {
         let randomIndex = Math.floor(Math.random() * passOptions.length);
         password += passOptions[randomIndex];
     }
+    displayPassword();
     return password;
 }
 
@@ -36,14 +40,25 @@ passLenSlider.addEventListener("input", (e) => {
 
 document.querySelector("#include-num").addEventListener("click", (e) => {
     numAllowed = e.target.checked;
+    generatePassword();
 });
 
 document.querySelector("#include-upper").addEventListener("click", (e) => {
     upperCaseAllowed = e.target.checked;
+    generatePassword();
 });
 
 document.querySelector("#include-symbol").addEventListener("click", (e) => {
     symbolsAllowed = e.target.checked;
+    generatePassword();
+});
+
+document.querySelector(".re-generate-btn").addEventListener("click", () => {
+    generatePassword();
 });
 
 displayPassLength(passLenSlider.value);
+generatePassword();
+document.querySelector("#include-num").checked = false;
+document.querySelector("#include-upper").checked = false;
+document.querySelector("#include-symbol").checked = false;
